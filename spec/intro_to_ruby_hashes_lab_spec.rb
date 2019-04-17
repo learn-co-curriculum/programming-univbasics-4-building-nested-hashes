4e1fb71aa9f1d25afa349ca6b966770998f2ad5c
maxwellbenton <maxwellbenton@gmail.com>
2019-04-17 07:28:58 -0400

    removes tests for non-nested hash lab portion

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -1,26 +1,7 @@
 require_relative 'spec_helper'
 require_relative '../intro_to_ruby_hashes_lab.rb'
 
-describe "working with hashes" do
 
-  describe "instantiating" do
-    describe "#new_hash" do
-      it "creates an empty hash and returns it" do
-
-        expect(new_hash).to eq({})
-      end
-    end
-  end
-
-  describe "instantiating a hash with ONE key-value pair" do
-    describe "#actor" do
-      it "creates a hash named actor whose key is a symbol :name and whose value is a string, 'Dwayne The Rock Johnson'" do
-        expect(actor[:name]).to eq("Dwayne The Rock Johnson")
-      end
-    end
-  end
-
-end
 
 describe "building a multidimensional monopoly hash" do
   describe "adding a key-value pair to the top level of the monopoly hash" do
