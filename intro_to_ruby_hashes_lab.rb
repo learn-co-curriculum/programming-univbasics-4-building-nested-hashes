57cd788a2baaca89257ab7af47c56d533fd7db07
Steven G. Harms <steven.harms@flatironschool.com>
2019-07-30 13:56:17 -0400

    Rename base method: confusing
    monopoly returning an lvar monopoly (which is bad
    Ruby style to not use that name anyway) is
    confusing.
    
    Also prompts a reminder of implicit return

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,7 @@
-def monopoly
-	monopoly = {}
-
+def base_hash
+  # Remember implicit return! We're returning this new Hash _without_ the
+  # keyword "return." Nice and neat.
+	{}
 end
 
 def monopoly_with_second_tier
