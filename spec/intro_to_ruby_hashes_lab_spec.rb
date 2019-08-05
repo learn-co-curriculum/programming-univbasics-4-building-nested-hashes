54a6b11d209c927719dcc7c42beb10eb3f530608
Steven G. Harms <steven.harms@flatironschool.com>
2019-08-05 07:11:13 -0400

    Update intro_to_ruby_hashes_lab_spec.rb

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -103,7 +103,7 @@ describe "We can create complex, nested Hashes by implementing four methods:" do
       expect(monopoly_with_fourth_tier[:railroads][:names][:b_and_o_railroad]["mortgage_value"]).to eq("$400")
     end
 
-    it "sets the 1st key of the :shortline_railroad hash to a string, 'mortgage_value', whose value is also a string, '$400'" do
+    it "sets the 1st key of the :shortline_railroad hash to a string, 'mortgage_value', whose value is also a string, '$800'" do
       expect(monopoly_with_fourth_tier[:railroads][:names][:shortline_railroad]["mortgage_value"]).to eq("$800")
     end
   end
