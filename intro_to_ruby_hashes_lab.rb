de14c943ad53bcfc1a777859b20dd224c58955d6
maxwellbenton <maxwellbenton@gmail.com>
2020-03-05 15:55:43 -0500

    set starter code

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
+
 end
 
 def monopoly_with_second_tier
@@ -9,6 +9,7 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+
 end
 
 def monopoly_with_third_tier
@@ -16,6 +17,7 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+
 end
 
 def monopoly_with_fourth_tier
@@ -23,4 +25,5 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+
 end
