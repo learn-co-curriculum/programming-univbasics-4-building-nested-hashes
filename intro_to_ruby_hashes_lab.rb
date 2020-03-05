4b6b4dec67dba1673243f75dde5eb3a65dd2e27d
maxwellbenton <maxwellbenton@gmail.com>
2020-03-05 16:04:45 -0500

    add new solution

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,9 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-
+  {
+    railroads: {}
+  }
 end
 
 def monopoly_with_second_tier
@@ -9,7 +11,13 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  {
+    railroads: {
+      pieces: 4,
+      rent_in_dollars: {},
+      names: {}
+    }
+  }
 end
 
 def monopoly_with_third_tier
@@ -18,6 +26,23 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 
+  {
+    railroads: {
+      pieces: 4,
+      rent_in_dollars: {
+        one_piece_owned: 25,
+        two_pieces_owned: 50,
+        three_pieces_owned: 100,
+        four_pieces_owned: 200,
+      },
+      names: {
+        reading_railroad: {},
+        pennsylvania_railroad: {},
+        b_and_o_railroad: {},
+        shortline_railroad: {},
+      }
+    }
+  }
 end
 
 def monopoly_with_fourth_tier
@@ -25,5 +50,29 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  {
+    railroads: {
+      pieces: 4,
+      rent_in_dollars: {
+        one_piece_owned: 25,
+        two_pieces_owned: 50,
+        three_pieces_owned: 100,
+        four_pieces_owned: 200,
+      },
+      names: {
+        reading_railroad: {
+          mortgage_value: 100
+        },
+        pennsylvania_railroad: {
+          mortgage_value: 200
+        },
+        b_and_o_railroad: {
+          mortgage_value: 400
+        },
+        shortline_railroad: {
+          mortgage_value: 800
+        },
+      }
+    }
+  }
 end
